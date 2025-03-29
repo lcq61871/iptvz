74afb5bd1a7d94a404213b8ee60b1d668eb24142
lcq61871 <95287580+lcq61871@users.noreply.github.com>
2025-03-29 11:00:45 +0800

    Update script.py
    Signed-off-by: lcq61871 <95287580+lcq61871@users.noreply.github.com>

--- script.py
+++ script.py
@@ -1,18 +1,18 @@
 import requests
 
-def fetch_and_filter():
-    url = 'https://raw.githubusercontent.com/luoye20230624/ZB/refs/heads/main/iptv_list.txt'
-    
+def fetch_and_filter(url, output_file):
     # 获取文件内容
     response = requests.get(url)
+    response.raise_for_status()  # 检查请求是否成功
     content = response.text
     
     # 过滤掉包含 "ipv6" 的行
     filtered_lines = [line for line in content.splitlines() if 'ipv6' not in line.lower()]
     
     # 保存到新文件
-    with open('live_ipv4.txt', 'w') as file:
+    with open(output_file, 'w', encoding="utf-8") as file:
         file.write('\n'.join(filtered_lines))
 
 if __name__ == "__main__":
-    fetch_and_filter()
+    fetch_and_filter('https://raw.githubusercontent.com/luoye20230624/ZB/refs/heads/main/iptv_list.txt', 'live_ipv4.txt')
+    fetch_and_filter('https://raw.githubusercontent.com/ngdikman/hksar/refs/heads/main/dianxin.txt', 'dx.txt')
