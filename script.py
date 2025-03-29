3154ef6d4ab7ebe15cec094d4001cae00f73fe17
lcq61871 <95287580+lcq61871@users.noreply.github.com>
2025-03-29 09:59:07 +0800

    Update script.py
    Signed-off-by: lcq61871 <95287580+lcq61871@users.noreply.github.com>

--- script.py
+++ script.py
@@ -10,13 +10,10 @@ def fetch_and_filter():
     # 过滤掉包含 "ipv6" 的行
     filtered_lines = [line for line in content.splitlines() if 'ipv6' not in line.lower()]
     
-    # 保存到新文件
-    with open('live_ipv4.txt', 'w') as file:
-        file.write('\n'.join(filtered_lines))
 
 # 合并所有的txt文件
 file_contents = []
-file_paths = ["live_ipv4.txt", "AKTV.txt", "maotv.txt"]  # 替换为实际的文件路径列表
+file_paths = ["iptv_list.txt'", "AKTV.txt", "maotv.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
