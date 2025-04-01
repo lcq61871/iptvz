9d81f9ae4d220a2fe62bab16753d46cf7c0f3de8
lcq61871 <95287580+lcq61871@users.noreply.github.com>
2025-04-01 18:11:12 +0800

    Update script.py
    Signed-off-by: lcq61871 <95287580+lcq61871@users.noreply.github.com>

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import requests
 
 def fetch_and_filter():
-    url = 'https://raw.githubusercontent.com/luoye20230624/ZB/refs/heads/main/iptv_list.txt'
+    url = 'https://raw.githubusercontent.com/luoye20230624/hndxzb/refs/heads/main/iptv_list.txt'
     
     # 获取文件内容
     response = requests.get(url)
